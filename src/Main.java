754ff981e4833f1d62c2fa4f244df1876357c4f4
Brandon Piña <brandonpi265@gmail.com>
2023-02-06 20:22:03 -0600

    Picasso

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,22 @@
 import Module3.ArrayQueue;
 import Module5.BST;
 import Module6.MinHeap;
+import Module7.ExternalChainingHashMap;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BST<Integer> foo = new BST<>();
+        ExternalChainingHashMap<Integer, Integer> foo = new ExternalChainingHashMap<>();
+        var table = foo.getTable();
 
-    foo.add(1);
-    foo.add(0);
-    foo.add(2);
-    foo.add(3);
-        foo.remove(1);
-        MinHeap<Integer> bar = new MinHeap<>();
+        foo.put(0,0);
+        foo.put(13,13);
+        foo.put(13*2,13*2);
 
-
-        for(Integer i = 20; i > 0;i--){
-            bar.add(i*2);
-        }
-
-        while(true){
-            Integer x = bar.remove();
-            System.out.println(x);
-        }
-
+        foo.remove(13);
+        foo.getTable();
     }
-
-
-
-
 }
 
 
