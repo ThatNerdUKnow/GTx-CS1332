f0dfd2253a87b8db42bf50d9f166c1b1c9c0356b
Brandon Piña <brandonpi265@gmail.com>
2023-02-14 18:23:45 -0600

    AVL Tree balancing operations

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,24 @@ import Module3.ArrayQueue;
 import Module5.BST;
 import Module6.MinHeap;
 import Module7.ExternalChainingHashMap;
+import Module8.AVL;
+import Module8.AVLNode;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        ExternalChainingHashMap<Integer, Integer> foo = new ExternalChainingHashMap<>();
-        var table = foo.getTable();
+        AVL<Integer> AVL = new AVL<>();
+        AVLNode<Integer> A = new AVLNode<>(1);
+        AVLNode<Integer> B = new AVLNode<>(3);
+        AVLNode<Integer> C = new AVLNode<>(2);
 
-        foo.put(0,0);
-        foo.put(13,13);
-        foo.put(13*2,13*2);
+        A.setRight(B);
+        B.setLeft(C);
 
-        foo.remove(13);
-        foo.getTable();
+        AVL.balance(C);
+        AVL.balance(B);
+        AVL.balance(A);
     }
 }
 
